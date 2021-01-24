ae34cfc91dfaef9337c5e9e8b7ab3f906b5f14ac
akakcolin <akakcolin@163.com>
2021-01-24 15:32:07 +0800

    t

---
+++ static/js/custom.js
@@ -0,0 +1,42 @@
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function set_home_page_all_views(hit) {
+    document.getElementById("all-page-views").textContent = hit;
+}
+
+let allPageViewsAPI = "https://api.bmpi.dev/page-views/bmpi-dev-all-page-views/";
+let singlePageViewsAPI = "https://api.bmpi.dev/page-views/bmpi.dev" + window.location.pathname;
+
+function get_post_views(url, callback) {
+    httpGetAsync(url, callback);
+}
+
+function main() {
+    if (window.location.pathname === "/") {
+        get_post_views(allPageViewsAPI, res => {
+            set_home_page_all_views(JSON.parse(res).count);
+        });
+        get_post_views(singlePageViewsAPI, res => {});
+    } else {
+        postViews = document.getElementById("post-views");
+        if (postViews != undefined) {
+            get_post_views(allPageViewsAPI, res => {});
+            get_post_views(singlePageViewsAPI, res => {
+                postViews.textContent = JSON.parse(res).count;
+            });
+        } else {
+            get_post_views(allPageViewsAPI, res => {});
+            get_post_views(singlePageViewsAPI, res => {});
+        }
+    }
+}
+
+main();
